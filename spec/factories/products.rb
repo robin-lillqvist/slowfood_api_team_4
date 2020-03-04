dbe4336a791daad8ef22ec168f45c0446936165e
Janko <mailodjanka@gmail.com>
2020-03-04 13:29:35 +0100

    Defiened a FactoryBot for a product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :product do
-    name { "MyString" }
-    description { "MyText" }
-    price { "" }
+    name { "Cheeseburger" }
+    description { "Bun, meat and cheese" }
+    price { "100" }
   end
 end
