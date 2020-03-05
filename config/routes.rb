beb68e266ceb8b1ad64f91bfeb1681c2a4ad9b25
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-05 10:32:06 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
     namespace :v1 do
-          end
+
+    end
   end
 end
