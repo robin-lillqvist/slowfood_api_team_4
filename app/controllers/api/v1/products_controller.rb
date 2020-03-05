ce69ca3abb032299ff512184e631f297b87c1282
Janko <mailodjanka@gmail.com>
2020-03-05 10:57:32 +0100

    Fixing formating + deleting v0 controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ProductsController < ApplicationController
-
     def index
         product = Product.all
         
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-
-    namespace :v0 do
-      resources :pings, only: [:index], constraints: { format: 'json' }
-    end
-
     namespace :v1 do
       resources :products, only:[:index]
     end
-
   end
 end
